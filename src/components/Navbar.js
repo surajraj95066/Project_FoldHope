d7e71ec814d26cf1b4612cbf55612dab67842c4e
surajraj95066 <166308823+surajraj95066@users.noreply.github.com>
2025-04-08 22:05:40 +0530

    Updated GitHub Pages deployment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,6 @@ export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                {/* <a className="navbar-brand" href="https://surajraj95066.github.io/project-react/">FoldHope</a> */}
                 <Link to="/" className="nav-link">FoldHope</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
